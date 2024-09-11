4bc737b06775b8ab76f29484e644e89b2f1c4cdf
jjh1214 <scarlet777735@gmail.com>
2024-09-11 15:40:18 +0900

    Add rviz launch

---
+++ pge_50_26_description/setup.py
@@ -0,0 +1,31 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'pge_50_26_description'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.urdf')),
+        (os.path.join('share', package_name, 'meshes', 'pge_50_26'), glob('meshes/pge_50_26/*.dae')),
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='roboarm',
+    maintainer_email='scarlet777735@gmail.com',
+    description='TODO: Package description',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
